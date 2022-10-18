d4706d3aa46461a55ba1a57ddbb8cf650af9f037
zvhen <zn.vhen@gmail.com>
2022-10-18 23:13:54 +0800

    My triangle solution

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,10 +1,24 @@
 def equilateral(sides):
-    pass
-
+    if triangle_inequality(sides) and sides[0]*sides[1]*sides[2]> 0:
+        return sides[0]==sides[1]==sides[2]
+    else:
+        return False
 
 def isosceles(sides):
-    pass
-
+    if triangle_inequality(sides) and sides[0]*sides[1]*sides[2]> 0:
+        return sides[0]==sides[1] or sides[0]==sides[2] or sides[1]==sides[2]
+    else:
+        return False
 
 def scalene(sides):
-    pass
+    if triangle_inequality(sides) and sides[0]*sides[1]*sides[2]> 0:
+        return sides[0]!=sides[1] and sides[0]!=sides[2] and sides[1]!=sides[2]
+    else:
+        return False
+    
+def triangle_inequality(sides):
+    if sides[0] + sides[1] >= sides[2] and sides[1] + sides[2] >= sides[0] and sides[0] + sides[2] >= sides[1]:
+        return True
+    else:
+        False
+
