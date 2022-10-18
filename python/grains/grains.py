080de5dace6e749038898117281e736fc1be3231
Chang Zhen Vhen <zn.vhen@gmail.com>
2022-10-18 11:55:18 +0800

    Updated grains.py code to be more efficient, removed redundancies

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,31 +1,8 @@
-import timeit
-
 def square(number):
-    try:
-        if number <= 0 or number > 64:
-            raise ValueError("square must be between 1 and 64")
-        return 2**(number-1)
-    except ValueError as ve:
+    if number not in range(1,65):
         raise ValueError("square must be between 1 and 64")
-    pass
-
+    return 2**(number-1)
 
 def total():
-    number = 1
-    summ = 0
-    try:
-        while number <= 64:
-            summ += 2**(number-1)
-            number += 1
-    except ValueError:
-        raise ValueError("square must be between 1 and 64")
+    return (2**64) -1
 
-    return summ
-    pass
-
-
-#print(square(-6))
-#start = timeit.timeit()
-#print(total())
-#end = timeit.timeit()
-#print(end-start)
