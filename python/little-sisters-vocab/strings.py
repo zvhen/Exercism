faa93a1e5bb62509967bf1f7c24045ca289b57b1
Chang Zhen Vhen <zn.vhen@gmail.com>
2022-10-26 11:49:03 +0800

    Download exercism strings.py

--- python/little-sisters-vocab/strings.py
+++ python/little-sisters-vocab/strings.py
@@ -5,30 +5,28 @@ def add_prefix_un(word):
     return "un"+word
 
 def make_word_groups(vocab_words):
-    return ''.join(vocab_words[0], words) for words in vocab_words
+    prefix = vocab_words[0]
+    new_list = prefix + (' :: '+prefix).join(vocab_words[1:])
+    return prefix + ' :: ' + new_list
 
-print(make_word_groups(['pre', 'serve', 'dispose', 'position']))
 
 def remove_suffix_ness(word):
-    """Remove the suffix from the word while keeping spelling in mind.
-
-    :param word: str - of word to remove suffix from.
-    :return: str - of word with suffix removed & spelling adjusted.
-
-    For example: "heaviness" becomes "heavy", but "sadness" becomes "sad".
-    """
-
-    pass
+    suffix = word.split('ness')[0]
+    if suffix[-1] != 'i':
+        return suffix
+    else:
+        return suffix.replace(suffix[-1],'y')
 
 
 def adjective_to_verb(sentence, index):
-    """Change the adjective within the sentence to a verb.
+    split_sentence = sentence.split()
+    if split_sentence[index][-1] == '.':
+        return split_sentence[index][:-1] + 'en'
+    else:
+        return split_sentence[index] + 'en'
 
-    :param sentence: str - that uses the word in sentence.
-    :param index: int - index of the word to remove and transform.
-    :return: str - word that changes the extracted adjective to a verb.
+#    new_sentence = list(map(lambda x: x.replace(split_sentence[index], verb), split_sentence))
+    
+#   return ' '.join(new_sentence)
 
-    For example, ("It got dark as the sun set", 2) becomes "darken".
-    """
 
-    pass
